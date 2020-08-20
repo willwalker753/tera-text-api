79611933595a1fa3c75fe517abb4fbc371822a03
Will Walker <williamwalker753@gmail.com>
2020-08-20 16:26:57 -0500

    delete friend and get all friend

--- src/routes/friend.js
+++ src/routes/friend.js
@@ -13,11 +13,9 @@ const pool = new Pool({
 
 router.post('/addcode', async (req, res) => {
     try {
-        //console.log(req.body)
         let sql = 'SELECT id, username FROM users WHERE id=$1';
         let params = [ req.body.friendCode ];
         let response = await pool.query(sql, params);
-        //console.log(response);
         if(response.rows[0].id === undefined) {
             res.send('no matches');
         }
@@ -35,6 +33,49 @@ router.post('/addcode', async (req, res) => {
     }
     catch {
         res.send('Account not found')
+    }
+})
+router.post('/addusername', async (req, res) => {
+    try {
+        let sql = 'SELECT id, username FROM users WHERE username=$1';
+        let params = [ req.body.friendUsername ];
+        let response = await pool.query(sql, params);
+        if(response.rows[0].username === undefined) {
+            res.send('no matches');
+        }
+        else if(response.rows[0].username){
+            sql = 'INSERT INTO '+req.body.username+'(friendid, friendusername) VALUES($1, $2) RETURNING *';
+            params = [ response.rows[0].id, response.rows[0].username ];
+            responseAddFriend = await pool.query(sql, params);
+            
+            sql = 'INSERT INTO '+response.rows[0].username+'(friendid, friendusername) VALUES($1, $2) RETURNING *';
+            params = [ req.body.userId, req.body.username ];
+            responseAddFriend = await pool.query(sql, params);
+        }
+        res.send(response.rows);
+        
+    }
+    catch {
+        res.send('Account not found')
+    }
+})
+router.post('/removefriend', async (req, res) => {
+    try {
+        console.log(req.body)
+        let sql = 'DELETE FROM '+req.body.username+' WHERE friendusername=$1';
+        let params = [ req.body.usernameToRemove ];
+        let response = await pool.query(sql, params);
+        console.log(response.rows)
+        
+        sql = 'DELETE FROM '+req.body.usernameToRemove+' WHERE friendusername=$1';
+        params = [ req.body.username ];
+        responseRemoveFriend = await pool.query(sql, params);
+        res.send(response.rows);
+        
+        
+    }
+    catch {
+        res.send('Trouble connecting to db')
     }
 })
 router.post('/all', async (req, res) => {
